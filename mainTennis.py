6bcc1f6b77b93da26835d2f8abdf1fd9162ea45c
Borut <borut.znidar@gmail.com>
2014-11-18 21:04:15 +0100

    corrections of UserEdit. Not checked yet

--- mainTennis.py
+++ mainTennis.py
@@ -281,6 +281,10 @@ def EditUser():
             return render_template("listUsers.html", users=users )
             
     elif request.method == 'POST':
+        if request.form["Status"] == "Shrani":
+            u = User( username=request.form["username"], utype=request.form["utype"], 
+                      active=request.form["active"], email=request.form["email"])
+            u.update( request.form["ident"] )
         return redirect(url_for("TennisMain"))
 
 
