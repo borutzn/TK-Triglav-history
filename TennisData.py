cd9c745ae37d9c28db188f84f9632e7355da859f
Borut <borut.znidar@gmail.com>
2015-01-25 17:46:55 +0100

    small corrections

--- TennisData.py
+++ TennisData.py
@@ -252,7 +252,7 @@ class TennisEvent:
         if event_filter != "":
             search = 1 if event_filter.isalnum() else 2
             log_info("SEARCH=" + str(search))
-        while (len(events) < page_len) and (pos < len(cls.EventsCache)):
+        while (len(events) < page_len) and (pos < len(cls.EventsCache)-1):
             pos += 1
             if (search == 1) and (event_filter not in cls.EventsCache[pos]['Event']):
                 continue
