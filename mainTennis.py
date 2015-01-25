a47cddabb605322e07a5d303981bd9bd934cb7e8
Borut <borut.znidar@gmail.com>
2015-01-25 18:30:38 +0100

    small corrections

--- mainTennis.py
+++ mainTennis.py
@@ -256,9 +256,8 @@ def tennis_main():
 
     events = TennisEvent.get_events_page(pos, page_len=PAGELEN, event_filter=event_filter, collapsed_groups=())
     events_len = TennisEvent.count()
-    log_info("EVENTS: "+str(len(events)))
     if (len(events) == 0):
-        flash(u"Noben dogodek ne ustreza iskanju.")
+        flash(u"Noben dogodek ne ustreza.")
         return redirect(url_for("tennis_main"))
 
     return render_template("main.html", events=events, production=Production,
