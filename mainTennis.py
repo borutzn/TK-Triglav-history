1798385f0408d20c3a77c96b14171eb0bbba730a
Borut <borut.znidar@gmail.com>
2015-01-29 17:02:47 +0100

    small corrections

--- mainTennis.py
+++ mainTennis.py
@@ -211,7 +211,8 @@ def correct():
             fname = request.args.get('f')
             next_pg = request.args.get('next')
         except (ValueError, TypeError) as e:
-            #  return redirect(url_for("tennis_main"))
+            log_info("ERROR: %s" % str(e))
+            # return redirect(url_for("tennis_main"))
             return redirect(request.args.get("next") or url_for("tennis_main"))
 
         fnames = []
