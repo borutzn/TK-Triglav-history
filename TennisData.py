ecb6ba4bb8e9c23143bb72caa3ca6d19ceb1947d
Borut <borut.znidar@gmail.com>
2015-02-06 17:08:09 +0100

    list && edit files

--- TennisData.py
+++ TennisData.py
@@ -287,8 +287,9 @@ class TennisEvent:
             if (ev['Date'][:4] != att[:4]):
                 continue
             # log_info(ev['Date'][:4]+":"+att[:4])
-            log_info(att[5:]+" -> Att1:"+ev['Att1'])
-            if (ev['Att1'] == att[5:]) or (ev['Att2'] == att[5:]) or (ev['Att3'] == att[5:]) or (ev['Att4'] == att[5:]):
+            att = att[5:]
+            log_info(str(att==ev['Att1'])+" - "+att+" -> Att1:"+ev['Att1'])
+            if (ev['Att1'] == att) or (ev['Att2'] == att) or (ev['Att3'] == att) or (ev['Att4'] == att):
                 log_info("%s - %s" % (ev['LocalDate'], ev['Event']))
                 r.append("%s - %s" % (ev['LocalDate'], ev['Event']))
         return r
