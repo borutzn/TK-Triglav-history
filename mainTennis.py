444efadd3122578e4ea86afcf5c142266468a31f
Borut <borut.znidar@gmail.com>
2015-05-23 22:03:48 +0200

    switch to "one year on a page"

--- mainTennis.py
+++ mainTennis.py
@@ -276,7 +276,6 @@ def tennis_main():
     year_len = len(TennisEvent.Years)
     year_idx = TennisEvent.Years.index(year if year else TennisEvent.Years[0])
     for s in TennisEvent.sources:
-        log_info("src:%s" % str(s))
         if s[0] == year:
             log_info("src:%s, %d" % (s[1], s[4]))
     return render_template("main.html", events=events, production=Production,
