820b4650168928acff4d16ce1fc232be94e03b64
Borut <borut.znidar@gmail.com>
2015-07-13 20:46:01 +0200

    mali popravki

--- mainTennis.py
+++ mainTennis.py
@@ -159,7 +159,7 @@ def add_event(step):
                              att1=request.form["att1"], att2=request.form["att2"],
                              att3=request.form["att3"], att4=request.form["att4"],
                              comment=request.form["comment"])
-            log_info("Before put1 ev=" % str(ev))
+            log_info("Before put1 ev=%s" % str(ev))
             ev.put(fetch=False)
             log_info("Put1")
             for p in range(2, 7):
@@ -167,7 +167,7 @@ def add_event(step):
                     ev.category = request.form["category%d" % p]
                     ev.result = request.form["result%d" % p]
                     ev.player = request.form["player%d" % p]
-                    log_info("Before put ev=" % str(ev))
+                    log_info("Before put ev=%s" % str(ev))
                     ev.put(fetch=False)
                     log_info("After put%d" % p)
             TennisEvent.fetch_data(force=True, sources=False)
