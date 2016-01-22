cfbf9e1d633aa54c7192c4551619b7f5cebd45b8
Borut <borut.znidar@gmail.com>
2016-01-22 20:38:54 +0100

    nov UI - Events page

--- mainTennis.py
+++ mainTennis.py
@@ -348,7 +348,7 @@ def tennis_events_year():
     if request.method == 'GET':
         try:
             year = request.args.get('y')
-            log_info("0")
+            log_info("X")
             log_info("0 %s" % str(request.args))
             log_info("1 %s." % str(year))
             if year not in TennisEvent.Years:
