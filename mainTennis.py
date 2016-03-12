169915650d1b6738821490f4245991d07330d3e8
Borut <borut.znidar@gmail.com>
2016-03-12 13:02:26 +0100

    nov UI - Players page

--- mainTennis.py
+++ mainTennis.py
@@ -345,9 +345,8 @@ def tennis_events():
         log_info("Error: GET / - no event")
         return redirect(request.args.get("next") or url_for("tennis_main1"))
 
-    i = TennisEvent.Years.index(year)
-    prev_y = TennisEvent.Years[i-1 if i > 0 else 0]
     i = TennisEvent.Years.index(events[0][1]['Date'][:4])
+    prev_y = TennisEvent.Years[i-1 if i > 0 else 0]
     next_y = TennisEvent.Years[i+1 if i < len(TennisEvent.Years)-1 else 0]
     if player_name:
         player = TennisPlayer.get(player_name)
