81bbd224c20cf33964944f70cade99a11e8b5787
Borut <borut.znidar@gmail.com>
2016-04-14 19:28:22 +0200

    small changes

--- mainTennis.py
+++ mainTennis.py
@@ -320,7 +320,6 @@ def tennis_main():
 def tennis_events():
     log_info("METHOD: %s" % request.method)
     log_info("Args (%d) %s" % (len(request.args), str(request.args)))
-    log_info("Form (%d) %s" % (len(request.form), str(request.form)))
     if request.method == 'GET':
         try:
             year = request.args.get('y') or TennisEvent.Years[0]
